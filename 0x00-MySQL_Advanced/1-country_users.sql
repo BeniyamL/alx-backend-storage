95d8d4add1053e2de696b476718e4be62e7fbae0
Beniyam Legesse <lbeniyam@gmail.com>
2021-12-08 17:51:05 +0300

    advanced mysql task from 0-10

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,10 @@
+-- create table with enumeration type
+-- make id primary key & auto increment
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL,
+    PRIMARY KEY (id)
+);
---
+++ README.md
@@ -0,0 +1,3 @@
+# ALX Backend Storage
+This is a place to advanced the Basic SQL knowledge. In this folder you are going to explore advanced sql concepts.
+Have fun!!!
