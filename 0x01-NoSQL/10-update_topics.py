a7ce2f685396f50423a782f37b767b04fda15439
Beniyam Legesse <lbeniyam@gmail.com>
2021-12-14 09:10:29 +0300

    mongo from task 0 - 12

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import pymongo
+""" a python module to change all topics of a school document"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    update_topics - function to update topicsof a school
+    Arguments:
+        mongo_collection: the given mongo collection
+        name: the school name to be updated
+        topics: list of topics
+    Returns:
+        nothing
+    """
+    rqrd_attr = {"name": name}
+    updt_attr = {"$set": {"topics": topics}}
+    mongo_collection.update_many(rqrd_attr, updt_attr)
